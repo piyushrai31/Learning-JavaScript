5d51c03ba0042460413d11073474f7b9eae31a9b
Piyush Rai <piyushraivns31@gmail.com>
2026-05-25 13:46:59 +0530

    first JS Project

---
+++ 01_Basics/variables.js
@@ -0,0 +1,21 @@
+const id= 124543
+let userName = "piyush"
+var password= "134w42"
+city = "Varanasi" // should not be used preferrably
+
+// id= 124352
+
+// var has a problem of block scope and functional scope so better not to use it, try to use let always for variables
+
+userName = "Piyush Rai"
+password = "32443"
+city = "Jaipur"
+
+console.log(userName);
+console.table([userName,password,city])
+
+console.log(typeof null)
+console.log(typeof undefined);
+
+
+
